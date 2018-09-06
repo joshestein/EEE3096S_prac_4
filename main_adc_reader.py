0abd1962958971d906153d523cbb6c9756e71735
kelseykap <kplkel001@myuct.ac.za>
2018-09-06 15:18:48 +0200

    updated structure to match

--- main_adc_reader.py
+++ main_adc_reader.py
@@ -14,7 +14,7 @@ SPICS = 8
 
 LDR_channel = 7
 temp_sensor_channel = 0
-pot_adc = 5
+pot_channel = 5
 
 GPIO.setup(SPIMOSI, GPIO.OUT)
 GPIO.setup(SPIMISO, GPIO.IN)
@@ -27,17 +27,12 @@ mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
 def convert_voltage(data):
     #ldr needs to be proportioned to flashlight
     return (data*3.3)/float(1023)
-    
-def convert_temp(data):
-    temp = ((data*330)/float(1023))-5
-
-def readPot():
-    val = mcp.read_adc(pot_adc)
-    return (val/1024)*3.3
 
 while True:
     #temp_level = mcp.read_adc(temp_sensor_channel)
     ldr_level = mcp.read_adc(LDR_channel)
+    temp_level = mcp.read_adc(temp_sensor_channel)
+    pot_level = mcp.read_adc(pot_channel)
     print("{} V".format(convert_voltage(ldr_level)))
     time.sleep(0.5)
 
