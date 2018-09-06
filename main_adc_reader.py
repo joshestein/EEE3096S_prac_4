defde247150fcb07fe569fb2f28526584d072dd9
Josh Stein <josh.e.stein@gmail.com>
2018-09-06 16:01:32 +0200

    Added LDR percentages (#7)

--- main_adc_reader.py
+++ main_adc_reader.py
@@ -23,21 +23,36 @@ GPIO.setup(SPICS, GPIO.OUT)
 
 mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
 
-
 def convert_voltage(data):
     #ldr needs to be proportioned to flashlight
-    return (data*3.3)/float(1023)
+    return round((data*3.3)/float(1023), 2)
     
-def convert_temp(data):
-    temp = ((data*330)/float(1023))-5
+def get_temp_in_degrees(temp_voltage):
+    return (temp_voltage-500)/10
+
+def get_ldr_percentage(ldr_voltage):
+    #2.3 because there is a 1V drop across current limiting resistor
+    percentage = 100*(1-ldr_voltage/2.35)
+    if percentage > 0:
+        return round(percentage, 2)
+    else:
+        return 0
 
 def readPot():
     val = mcp.read_adc(pot_adc)
     return (val/1024)*3.3
 
 while True:
-    #temp_level = mcp.read_adc(temp_sensor_channel)
+    temp_level = mcp.read_adc(temp_sensor_channel)
+    temp_voltage = convert_voltage(temp_level)
+    temp = get_temp_in_degrees(temp_voltage)
+
     ldr_level = mcp.read_adc(LDR_channel)
-    print("{} V".format(convert_voltage(ldr_level)))
+    ldr_voltage = convert_voltage(ldr_level)
+    ldr_percentage = get_ldr_percentage(ldr_voltage)
+
+    #print("{}, {} V, {} C".format(temp_level, temp_voltage, temp))
+    print("{} V, {}%".format(ldr_voltage, ldr_percentage))
+
     time.sleep(0.5)
 
