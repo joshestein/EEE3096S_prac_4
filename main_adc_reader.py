d7cdb840c160879bdaa7f049879fa255645dce94
kelseykap <42800848+kelseykap@users.noreply.github.com>
2018-09-06 14:24:55 +0200

    added pot function (#4)
    * added pot function
    
    * defined adc

--- main_adc_reader.py
+++ main_adc_reader.py
@@ -14,6 +14,7 @@ SPICS = 8
 
 LDR_channel = 7
 temp_sensor_channel = 0
+pot_adc = 5
 
 GPIO.setup(SPIMOSI, GPIO.OUT)
 GPIO.setup(SPIMISO, GPIO.IN)
@@ -22,12 +23,17 @@ GPIO.setup(SPICS, GPIO.OUT)
 
 mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
 
+
 def convert_voltage(data):
     #ldr needs to be proportioned to flashlight
     return (data*3.3)/float(1023)
     
 def convert_temp(data):
     temp = ((data*330)/float(1023))-5
+
+def readPot():
+    val = mcp.read_adc(pot_adc)
+    return (val/1024)*3.3
 
 while True:
     #temp_level = mcp.read_adc(temp_sensor_channel)
