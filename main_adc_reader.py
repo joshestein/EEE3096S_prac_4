fa83b44a1555dc2122ef9587a6896910a6edde9b
Josh Stein <josh.e.stein@gmail.com>
2018-09-07 12:44:15 +0200

    Added reset button interrupt

--- main_adc_reader.py
+++ main_adc_reader.py
@@ -12,6 +12,8 @@ SPIMISO = 9
 SPIMOSI = 10
 SPICS = 8
 
+reset_btn = 16
+
 LDR_channel = 7
 temp_sensor_channel = 0
 pot_adc = 5
@@ -20,8 +22,25 @@ GPIO.setup(SPIMOSI, GPIO.OUT)
 GPIO.setup(SPIMISO, GPIO.IN)
 GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
+GPIO.setup(reset_btn, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+#callbacks
+def callback_reset(channel):
+    #clear screen
+    if os.name == 'nt':
+        #windows
+        os.system('clear')
+    else:
+        os.system('clear')
+
+    #TODO: reset timer (once timer function has been created)
+
+#interrupts
+GPIO.add_event_detect(reset_btn, GPIO.FALLING, callback=callback_reset, bouncetime=200)
 
 mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
+
+    
 
 def convert_voltage(data):
     #ldr needs to be proportioned to flashlight
@@ -51,8 +70,8 @@ while True:
     ldr_voltage = convert_voltage(ldr_level)
     ldr_percentage = get_ldr_percentage(ldr_voltage)
 
-    #print("{}, {} V, {} C".format(temp_level, temp_voltage, temp))
-    print("{} V, {}%".format(ldr_voltage, ldr_percentage))
+    print("{}, {} V, {} C".format(temp_level, temp_voltage, temp))
+    #print("{} V, {}%".format(ldr_voltage, ldr_percentage))
 
     time.sleep(0.5)
 
